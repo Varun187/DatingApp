fae2f3cb4671ae8fafd331ded0f1212c6ef32dce
vart <vart@publicisgroupe.net>
2023-08-17 15:55:38 +0530

    end of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
+
+//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
